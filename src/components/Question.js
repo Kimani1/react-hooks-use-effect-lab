343468d281ecfbf15c415eac7c057382f0886566
Mark <mark.kimani1@student.moringaschool.com>
2023-09-10 09:52:45 +0300

    complete

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,11 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    // This effect runs when the component renders
+    const timer = setTimeout(() => {
+      // This code decrements the timer by 1 every second
+      if (timeRemaining > 0) {
+        setTimeRemaining((prevTime) => prevTime - 1);
+      } else {
+        setTimeRemaining(10);
+        // Call onAnswered after 10 seconds
+        onAnswered(false);
+      }
+    }, 10);
+
+    return () => clearTimeout(timer);
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
+    // Reset the timer when the user answers
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
